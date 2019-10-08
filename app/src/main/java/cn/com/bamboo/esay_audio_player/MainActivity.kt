c058d0f836cea3b1b61b89a1ae00e5cd736d7dbc
yangx <yangx@edu-edu.com.cn>
2019-10-08 17:05:27 +0800

    创建项目

---
+++ app/src/main/java/cn/com/bamboo/esay_audio_player/MainActivity.kt
@@ -0,0 +1,12 @@
+package cn.com.bamboo.esay_audio_player
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
